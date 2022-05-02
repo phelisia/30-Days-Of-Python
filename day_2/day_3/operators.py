c3abbe3beb0c8a9998d09fad1db4215ab242d4a1
phelisa <phelisajeruto@gmail.com>
2022-05-02 06:27:13 +0300

    operators

--- day_2/day_3/operators.py
+++ day_2/day_3/operators.py
@@ -1,8 +1,83 @@
-#day3 operators
-age = 22
-height = 10.7
-basic_pay = 1902039.383839
-base = input("Enter base :")
-height = input("Enter height:")
-area = 3 * base * height
-print(area)
+#  Declare your age as integer variable 
+#  Declare your height as a float variable
+# Declare a variable that store a complex number
+from colorsys import TWO_THIRD
+
+
+age=23
+print(age)
+height=5.40
+print(height)
+complex=(5+5j)*(4-2j)
+# print(complex)
+# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+# base=int(input("Enter base:"  ))
+# height=int(input("Enter height: "))
+# area=base*height
+# print(f"the area os a triangl is base{base} multiply height{height} and  area is{area}")
+# # Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+# width=int(input("Enter width: "))
+# height=int(input("Enter height: "))
+# area=width*height
+# perimeter=(width)*2+(height)*2
+# print(f"area of a rectangle is{area} and perimeter is{perimeter}")
+# # Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+# radius=int(input("Enter radius: "))
+# pi=3.14
+# circum=pi*(radius)*2
+# area=2*pi*radius**2
+# print(f"area of a circle is {area} and circumfrence is {circum}")
+# # Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+# y1=int(input("Enter y1: "))
+# y2=int(input("Enter y2: "))
+# x1=int(input("Enter x1: "))
+# x2=int(input("Enter x2: "))
+# m=((((x2 - x1 )**2) + ((y2-y1)**2) )**2)
+# print(f"the distance between{x2,x1} and {y2,y1} is {m}")
+# Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+# y=x^2+6x+9
+# y=((x*x)_+(x*x*x*x*x*x))
+language="python"
+animal="dragon"
+print(len(language))
+print(len(animal))
+print("on"in language and animal)
+print("on" not in language and animal)
+sentence="I hope this course is not full of jargon"
+print("jargon" in sentence)
+print(len(language))
+print(float(len(language)))
+print(str(len(language)))
+# numbers=int(input("Enter number to test evenity:  " ))
+# if numbers%2==0:
+#     print(f"{numbers} number is even")
+# else:
+#     print(f"{numbers}odd number")
+numb_floor=7//3
+print(numb_floor)
+print(numb_floor==2.7)
+print(type('10')==type(10))
+# hours= int(input("Enter hours worked: "))
+# rate=int(input("Enter rate per hour"))
+# earning=hours*rate
+# print(f" your weekly earning is\t{earning}")
+years=int(input("Enter years you have lived: "))
+if years!=3.154e+7:
+    print( years*3.154e+7)
+else:
+    print("you already dead")
+numbers=int(input("Enter a multiple you like: "))
+for i in range (1,12):
+    print(numbers,'x',i,'=',numbers*i)
+     
+# Write a Python script that displays the following table
+# 1 1 1 1 1
+# 2 1 2 4 8
+# 3 1 3 9 27
+# 4 1 4 16 64
+# 5 1 5 25 125
+
+
+
+
+
